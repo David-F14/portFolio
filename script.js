c75e409f3b32b9c5f02cf5a87f30c7bee39fac71
David F14 <davidfalue@gmail.com>
2023-04-01 22:34:04 +0200

    finish

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 /** Change text of Home **/
 var typed = new Typed(".multiple-text", {
-    strings: ["C#", "Java", "Fullstack"],
-    typeSpeed: 200,
-    backSpeed: 200,
+    strings: ["C# Developer", "Java Develop", "Fullstack Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
     backDelay: 1000,
     loop: true    
 });
@@ -45,3 +45,16 @@ window.onscroll = () => {
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
 };
+
+/** scroll reveal **/
+ScrollReveal({ 
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
